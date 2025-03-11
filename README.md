33df7d0e6cadbfc397d446f726a0c9838470af35
dheeppr15lap <dheep@xps>
2025-03-12 00:45:24 +1100

    extract isbn number and page count. add to metadata

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pyebooklibrary
 Ebook Library written in Python
+
+## free api to find book details from ISBN number
+``` shell 
+curl https://openlibrary.org/api/books?bibkeys=ISBN:978-1-937785-33-8&jscmd=details&format=json
+```
+More apis on this link https://www.vinzius.com/post/free-and-paid-api-isbn/
---
+++ pdfextractmeta.py
@@ -0,0 +1,69 @@
+import pymupdf
+import re
+
+## Settings
+PAGES_TO_READ = 10
+# isbn_pattern = r"((978[-– ])?[0-9][0-9-– ]{10}[-– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])"
+# isbn_pattern = r'\b(?:\d{3}[- ]?)?\d{1,5}[- ]?\d{1,7}[- ]?\d{1,7}[- ]?\d{1,7}|\d{10}|\d{13}\b'
+isbn_pattern = r'ISBN.* (.*)'
+
+
+pdfsrcfile = "goodmath.pdf"
+# pdfsrcfile = "datascienceessentialsinpython.pdf"
+# pdfsrcfile = "practicalcybersecurityarchitecture.pdf"
+
+doc = pymupdf.open(pdfsrcfile) #open document
+metadata = doc.metadata
+pagecount = doc.page_count
+
+# add pagecount to meta data
+metadata['extratedpagecount'] = pagecount
+
+if pagecount > PAGES_TO_READ:
+    for i,page in enumerate(doc.pages(0,PAGES_TO_READ,1)):
+        print(f"{i}. Searching...")
+        # text = page.search_for("ISBN")
+        text = page.get_text()
+        print(text)
+
+        ## Find all ISBN numbers in the text
+        # isbn_numbers = re.findall(isbn_pattern, text)
+        gotisbn = re.search(isbn_pattern, text)
+    
+        ## Clean the ISBN numbers by removing hyphens or spaces
+        # cleaned_isbns = [isbn.replace('-', '').replace(' ', '') for isbn in isbn_numbers]
+
+        #print(isbn_numbers)
+        if gotisbn is not None:
+            print(gotisbn.groups())
+            isbn_num = gotisbn.groups()[0]
+            break
+        else:
+            isbn_num = None
+
+# add ISBN to metadata
+
+metadata['isbn'] = isbn_num
+
+
+print(f"Metadata of file {pdfsrcfile} is \n")
+[print(f"{item}: {metadata[item]} ") for item in metadata]
+
+# print(f"Page count {pagecount}")
+
+# if pagecount > PAGES_TO_READ:
+#     for i,page in enumerate(doc.pages(0,PAGES_TO_READ,1)):
+#         print(f"{i}. Searching...")
+#         # text = page.search_for("ISBN")
+#         text = page.get_text()
+
+#         # Find all ISBN numbers in the text
+#         isbn_numbers = re.findall(isbn_pattern, text)
+#         gotisbn = re.search(isbn_pattern, text)
+    
+#         # Clean the ISBN numbers by removing hyphens or spaces
+#         cleaned_isbns = [isbn.replace('-', '').replace(' ', '') for isbn in isbn_numbers]
+
+#         print(isbn_numbers)
+#         if gotisbn is not None:
+#             print(gotisbn.groups()[0])
